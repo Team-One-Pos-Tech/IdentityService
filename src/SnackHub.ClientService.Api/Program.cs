9afa39c202f9a6cd93592ad10770679348ff4ec2
Icaro Sousa <icaro.py@gmail.com>
2024-11-27 23:14:14 +0000

    ci: trigger ci

--- src/SnackHub.ClientService.Api/Program.cs
+++ src/SnackHub.ClientService.Api/Program.cs
@@ -10,9 +10,7 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
-
+        
         builder.Services.AddControllers();
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
@@ -27,8 +25,7 @@ public class Program
             .AddValidators();
 
         var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
+        
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
